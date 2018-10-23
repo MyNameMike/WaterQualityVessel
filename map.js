a8b006e429e2d28128cee267489f20e7f939adf7
Maikel <mikejc96@gmail.com>
2018-10-23 16:36:07 -0400

    10/23/18

--- map.js
+++ map.js
@@ -1,24 +1,25 @@
 function initMap() {
     var myLatLng = {lat: 25.774311, lng: -80.371441};
+    var markers = [];
   
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 18,
       center: myLatLng
     });
 
-    // var marker = new google.maps.Marker({
-    //     position: myLatLng,
-    //     map: map,
-    //     title: "Click to show location"
-    // });
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: "Click to show location"
+    });
 
-    // marker.addListener('click', function() {
-    //     infowindow.open(map, marker);
-    // });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
 
-    // var infowindow = new google.maps.InfoWindow({
-    // content: myLatLng.lat + "," + myLatLng.lng
-    // });
+    var infowindow = new google.maps.InfoWindow({
+    content: myLatLng.lat + "," + myLatLng.lng
+    });
 
     var myLoc;
 
@@ -27,13 +28,36 @@ function initMap() {
         myLoc = event.latLng;
      });
      
-     function placeMarker(location) {
+    function placeMarker(location) {
          var marker = new google.maps.Marker({
              position: location, 
-             map: map
+             map: map,
+             title: "Click to show location"
          });
-         alert('lat long: ' + myLoc);
-     }
-
-
+         markers.push(marker);
+         //alert('lat long: ' + myLoc);
+    }
 }
+    // Sets the map on all markers in the array.
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    // Clears all markers in array
+    function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+    // Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+    }
+
+    function exportMarkers() {
+
+    }
+
+
