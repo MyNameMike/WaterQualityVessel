aefd015d9275b444f59271ae338f8d307f8629eb
Maikel <mikejc96@gmail.com>
2018-10-25 19:20:31 -0400

    10/25/18

--- map.js
+++ map.js
@@ -1,43 +1,27 @@
-function initMap() {
-    var myLatLng = {lat: 25.774311, lng: -80.371441};
+    var map;
     var markers = [];
-  
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 18,
-      center: myLatLng
-    });
 
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: "Click to show location"
-    });
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 18,
+            center: {lat: 25.774311, lng: -80.371441},
+        });
 
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-    });
-
-    var infowindow = new google.maps.InfoWindow({
-    content: myLatLng.lat + "," + myLatLng.lng
-    });
-
-    var myLoc;
-
-    google.maps.event.addListener(map, 'click', function(event) {
-        placeMarker(event.latLng);
-        myLoc = event.latLng;
-     });
-     
-    function placeMarker(location) {
-         var marker = new google.maps.Marker({
-             position: location, 
-             map: map,
-             title: "Click to show location"
-         });
-         markers.push(marker);
-         //alert('lat long: ' + myLoc);
+        // This event listener will call addMarker() when the map is clicked.
+        map.addListener('click', function(event) {
+            addMarker(event.latLng);
+        });
     }
-}
+
+    // Adds a marker to the map and push to the array.
+    function addMarker(location) {
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+    markers.push(marker);
+    }
+
     // Sets the map on all markers in the array.
     function setMapOnAll(map) {
         for (var i = 0; i < markers.length; i++) {
@@ -45,10 +29,10 @@ function initMap() {
         }
     }
 
-    // Clears all markers in array
+    // Removes the markers from the map, but keeps them in the array.
     function clearMarkers() {
         setMapOnAll(null);
-      }
+    }
 
     // Deletes all markers in the array by removing references to them.
     function deleteMarkers() {
@@ -56,8 +40,18 @@ function initMap() {
         markers = [];
     }
 
+    // Exports markers' Lat,Lng in csv format 
     function exportMarkers() {
+        var csv = 'Latitude,Longitude\n';
+        for (let marker of markers) {
+            csv += marker.getPosition().lat() + "," + marker.getPosition().lng();
+            csv += "\n";
+        }
+        
 
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = 'map_data.csv';
+        hiddenElement.click();
     }
-
-
